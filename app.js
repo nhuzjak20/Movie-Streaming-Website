e9c8868a493717913a7a0a693c39df0c11abc8f9
nhuzjak20 <nhuzjak20@foi.hr>
2023-09-03 05:34:40 +0200

    Login i registracija rade

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ let db = new sqlite3.Database('./db/users.db',(err) => {
 app.use(express.static('css'))
 app.use(express.static('public'))
 app.use(cookieParser())
+app.set('view engine', 'ejs');
 
-db.all('select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME="Korisnici', (err, data) => {
-    if (err) console.log(err);
+db.all('select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME="Korisnici"', (err, data) => {
+    if (err) return;
     else console.log(data);
 })
 
@@ -35,16 +36,18 @@ db.all('select * from Korisnici',(err, results) => {
 
 //Ako vrati false korisnik ne postoji, ako vrati true onda postoji
 function ProvjeriKorisnika(user, password){
-    db.all('SELECT * FROM Korisnici WHERE username ="' + user + '" AND password="' + password + '"', (err, rows) =>{
+    db.all('SELECT * FROM Korisnici WHERE username ="' + user + '" AND pass="' + password + '"', (err, rows) =>{
         if(err) console.log(err);
-        else if(rows.length) return false;
-        else return true;
+        else if(rows.length) {
+            console.log(rows);
+            return true;
+        } else return false;
     })
 }
 
 //Ako vrati true, kolacic postoji
 function ProvjeriKolacic(id, user){
-    db.all('SELECT * FROM Korisnici WHERE kolacic=' + id + ' AND username=' + user, (err, rows) => {
+    db.all('SELECT * FROM Korisnici WHERE kolacic="' + id + '" AND username="' + user + '"', (err, rows) => {
         if(err) {
             console.log(err);
             return false; 
@@ -56,6 +59,8 @@ function ProvjeriKolacic(id, user){
 }
 
 
+
+
 app.get('/login',(req, res)=>{
     console.log("Login se upalio")
     res.sendFile(__dirname + '/html/login/login.html');
@@ -64,19 +69,44 @@ app.get('/login',(req, res)=>{
 app.post('/logiraj',urlencodedParser ,(req, res)=>{
     const username = req.body.username
     const pass = req.body.password
-    
+    console.log(username + " " + pass)
+    const kljuc = Date.now()
+    if(!ProvjeriKorisnika(username,pass)){
+        console.log("Login radi")
+        db.run('UPDATE Korisnici SET kolacic= ? where username = ?', [kljuc, username], (err, result) => {
+            if(err){
+                console.log(err)
+                res.redirect('/login?status=error')
+            } else {
+                res.cookie('uniqueID', kljuc)
+                res.cookie('user', username)
+                console.log("Kolacici postavljeni")
+                res.redirect('/')
+            }
+        })
+    } else {
+        res.redirect('/login?status=false')
+    }
 })
 
 app.get('/', (req, res)=>{
+    console.log('Spajanje na home')
     if(!req.cookies){
+        console.log('No cookies')
         res.redirect('/login');
     } else {
         console.log(req.cookies);
-        if(ProvjeriKolacic(req.cookies[0], req.cookies[1])) {
-            res.sendFile(__dirname + '/html/home/home.html');
-        }
+        if(!ProvjeriKolacic(req.cookies.uniqueID, req.cookies.username)) {
+            res.render('home', { username: req.cookies.user})
+        } else { res.redirect('/login?status=NoCookie'); }
     }
 })
+
+app.get('/odjava', (req, res) => {
+    res.clearCookie()
+    res.redirect('/login?status=LoggedOut')
+})
+
 
 app.post('/registriraj',urlencodedParser ,(req, res)=>{
     const username = req.body.username
