09baf47af969e57359f59f31815476bb24e9066a
nhuzjak20 <huzjakn5@gmail.com>
2025-02-05 17:14:44 +0100

    Napravljen login Controller

--- chat-app-server/src/app.module.ts
+++ chat-app-server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginControllerController } from './login-controller/login-controller.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, LoginControllerController],
   providers: [AppService],
 })
 export class AppModule {}
